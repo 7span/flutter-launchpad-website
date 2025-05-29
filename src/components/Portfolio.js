ff70742f686eb1ca221833f30bd6106b82c09334
Cavin <cavinmacwan1@gmail.com>
2025-05-29 19:10:19 +0530

    Update styles, add new logo, and modify portfolio subtitles
    - Changed border-radius of navbar links from 30px to 0px for a sharper look.
    - Added a new logo SVG file (logo-1.svg) to replace the old logo in the header.
    - Updated portfolio subtitles to better reflect the nature of each project.
    - Ensured portfolio links open in a new tab for better user experience.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,42 +4,42 @@ const portfolioData = [
   {
     id: 1,
     title: "Vepaar Store",
-    subtitle: "Web/WordPress",
+    subtitle: "Business",
     image: "assets/img/vepaar-store.png",
     link: "https://play.google.com/store/apps/details?id=com.whatshash",
   },
   {
     id: 2,
     title: "Abu Dhabi Pass",
-    subtitle: "Flutter",
+    subtitle: "Travel",
     image: "assets/img/abu-dabhi-pass-app.png",
     link: "https://play.google.com/store/apps/details?id=com.theabudhabipass.officialapp",
   },
   {
     id: 3,
     title: "Fotofizz",
-    subtitle: "Web/WordPress",
+    subtitle: "Social",
     image: "assets/img/fotofizz-app.png",
     link: "https://play.google.com/store/apps/details?id=com.fotofizz",
   },
   {
     id: 4,
     title: "CRM",
-    subtitle: "Web/WordPress",
+    subtitle: "Business",
     image: "assets/img/vepaar-crm.png",
     link: "https://play.google.com/store/apps/details?id=com.vepaar.crm",
   },
   {
     id: 5,
     title: "Voliz",
-    subtitle: "Web/WordPress",
+    subtitle: "Social",
     image: "assets/img/voliz-app.png",
     link: "https://play.google.com/store/apps/details?id=com.voliz",
   },
   {
     id: 6,
     title: "Liquify",
-    subtitle: "Web/WordPress",
+    subtitle: "Fintech",
     image: "assets/img/liquify-app.png",
     link: "https://play.google.com/store/apps/details?id=com.liquify",
   },
@@ -55,14 +55,22 @@ const Portfolio = () => {
             <div className="col-sm-6 col-lg-4" key={portfolio.id}>
               <div className="portfolio-box">
                 <div className="portfolio-img">
-                  <a href={portfolio.link} className="gallery-link">
+                  <a
+                    href={portfolio.link}
+                    className="gallery-link"
+                    target="_blank"
+                  >
                     <img src={portfolio.image} alt="image" />
                   </a>
                 </div>
                 <div className="portfolio-info">
                   <h6>{portfolio.title}</h6>
                   <span>{portfolio.subtitle}</span>
-                  <a href={portfolio.image} className="gallery-link">
+                  <a
+                    href={portfolio.link}
+                    className="gallery-link"
+                    target="_blank"
+                  >
                     <i className="fas fa-arrow-right" />
                   </a>
                 </div>
